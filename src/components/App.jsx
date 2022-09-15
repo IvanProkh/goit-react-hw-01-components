220d32e6fc74869154367b1d6d88d93dffb2b5ce
IvanProkhorets <prohorets.ivan@gmail.com>
2022-09-16 00:14:51 +0300

    add component User and other elements

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,18 @@
+import Profile from './user/User.jsx';
+import user from './user/user.json';
+
+console.log(Profile);
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
     </div>
   );
 };
