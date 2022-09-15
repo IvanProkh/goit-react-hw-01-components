73580d912ad1b5164b1484967846a0d055a95b7a
IvanProkhorets <prohorets.ivan@gmail.com>
2022-09-16 02:09:37 +0300

    update

--- src/components/user/User.jsx
+++ src/components/user/User.jsx
@@ -4,7 +4,7 @@ console.log('~ styles', styles);
 const Profile = ({ avatar, username, tag, location, stats }) => {
   return (
     <div className={styles.profile}>
-      <div class="description">
+      <div className={styles.description}>
         <img src={avatar} alt="User avatar" class="avatar" />
         <p class="name">{username}</p>
         <p class="tag">{tag}</p>
